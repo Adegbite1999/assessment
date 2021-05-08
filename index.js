73a1cc2f922e04309746b5fd0dd10e53466b91be
Adegbite Ademola <Adegbiteademola1999@gmail.com>
2021-05-08 15:53:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const hamburger = document.querySelector('.hamburger')
+const navUl = document.querySelector('#nav-ul')
+
+hamburger.addEventListener('click', () =>{
+    navUl.classList.toggle('show')
+})
+
+$(document).ready(()=> {
+
+   $('.register').on('mouseover',()=>{
+       $('.google').removeClass('hidden')
+       $('.overlay').removeClass('hidden')
+       $('.register').html('<i class="fas fa-user-plus"></i> Close')
+   }) 
+
+   $('.google').on('mouseout',()=>{
+    $('.google').addClass('hidden')
+    $('.overlay').addClass('hidden')
+    $('.register').html('<i class="fas fa-user-plus"></i> Sign Up')
+}) 
+$('.register').click(()=>{
+    $('.google').toggleClass('hidden')
+    $('.overlay').toggleClass('hidden')
+    $('.register').html(openAndclose())
+})
+
+
+function openAndclose(){
+    let ab = $('.register').html()
+
+return    (ab ==='<i class="fas fa-user-plus"></i> Sign Up' ) ? '<i class="fas fa-close"></i> Close' : '<i class="fas fa-user-plus"></i> Sign Up'
+}
+})
